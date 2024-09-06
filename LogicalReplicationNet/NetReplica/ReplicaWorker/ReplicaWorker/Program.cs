3748f5429107d5e87de5e9e491988e989880487c
kolotko111 <milosznowicki96@wp.pl>
2024-09-06 19:42:26 +0200

    logical replication net

---
+++ LogicalReplicationNet/NetReplica/ReplicaWorker/ReplicaWorker/Program.cs
@@ -0,0 +1,19 @@
+﻿using Npgsql.Replication;
+using Npgsql.Replication.PgOutput;
+
+await using var conn = new LogicalReplicationConnection("Server=localhost;Port=9097;Database=master_db;User Id=replica_user;Password=replica_user;");
+await conn.Open();
+
+var slot = new PgOutputReplicationSlot("blog_slot");
+
+// The following will loop until the cancellation token is triggered, and will print message types coming from PostgreSQL:
+var cancellationTokenSource = new CancellationTokenSource();
+await foreach (var message in conn.StartReplication(
+                   slot, new PgOutputReplicationOptions("blog_pub", 1), cancellationTokenSource.Token))
+{
+    Console.WriteLine($"Received message type: {message.GetType().Name}");
+
+    // Always call SetReplicationStatus() or assign LastAppliedLsn and LastFlushedLsn individually
+    // so that Npgsql can inform the server which WAL files can be removed/recycled.
+    conn.SetReplicationStatus(message.WalEnd);
+}
